6851547f72552264a3a1bc0dacb7b51a664c91c7
Hemsdale <babrier@gmail.com>
2026-04-29 17:06:08 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'stronglifts-v9';
+const CACHE = 'stronglifts-v9b';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
